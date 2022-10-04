42cd1dc0dd54341453e9c59c060251c0276c056d
Augusto Ojeda <okarinex@gmail.com>
2022-10-03 22:47:15 -0300

    card map

--- src/components/user/tasks/DetailsCardTask.tsx
+++ src/components/user/tasks/DetailsCardTask.tsx
@@ -1,7 +1,36 @@
-import { Box, Button, Card, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
+
+import React, {useState} from 'react'
+import { Box, Button, Card, CardContent, CardMedia, Checkbox, FormControlLabel, FormGroup, Typography } from '@mui/material'
+
+
+type FormValues = {
+    option1:  string;
+    option2:  string;
+    option3:  string;
+    option4:  string
+  };
+
+  const data = [{
+    img: "https://www.enter.co/wp-content/uploads/2021/07/Aliens-Newt-Ripley.jpeg",
+    pregunta: 'En la pelicula Alien como se llama la ni;a que rescada la comandate Ripley?',
+    opciones: [ {
+        a: 'josefa', 
+        b: 'manuela', 
+        c: 'lupita', 
+        d: 'abigail'}] 
+  }]
+
+  console.log(data)
 
 const DetailsCardTask = () => {
+
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setChecked(event.target.checked);
+    };
+
+
   return (
     <>
         <Box
@@ -36,6 +65,7 @@ const DetailsCardTask = () => {
                         <Typography
                             color={'white'}
                             fontSize={'15px'}
+                            component={'h3'}
                         >
                             N I V E L 1
                         </Typography>
@@ -50,21 +80,28 @@ const DetailsCardTask = () => {
                     sx={{display: 'flex', height: '85%', width: '100%', flexDirection: {xs: 'column', sm: 'row'}}}
                 >
 
-                    <CardMedia
-                          component="img"
-                          sx={{ width: { xs: '100%', sm: '60%'}, height: {xs: '50%', sm: '100%'}, objectFit: 'fill' }}
-                          image="https://www.enter.co/wp-content/uploads/2021/07/Aliens-Newt-Ripley.jpeg"
-                          alt="Ripley"
-                        />
+                    {
+                        data.map(e=>(
+                            <>
+                            <CardMedia
+                              component="img"
+                              sx={{ width: { xs: '100%', sm: '60%'}, height: {xs: '50%', sm: '100%'}, objectFit: 'fill' }}
+                              image={e.img}
+                              alt={e.img}
+                            />
+    
+                        <CardContent sx={{ width: { xs: '100%', sm: '40%'}, height: {xs: '50%', sm: '100%'} }}>
+                          <Typography
+                            
+                          >
+                            {e.pregunta}
+                          </Typography>
+                        </CardContent>
+                        </>
 
-                    <CardContent sx={{ flex: '1 0 auto', width: { xs: '100%', sm: '40%'}, height: {xs: '50%', sm: '100%'} }}>
-                      <Typography component="div" variant="h5">
-                        Live From Space
-                      </Typography>
-                      <Typography variant="subtitle1" color="text.secondary" component="div">
-                        Mac Miller
-                      </Typography>
-                    </CardContent>
+                        ))
+
+                    }
                 </Card>
             </Box>
 
