3f641d487dbb52fb991fa8dc5fe49d9838359fb7
Augusto Ojeda <okarinex@gmail.com>
2022-10-24 13:18:18 -0300

    hearders fixed

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -2,5 +2,10 @@ import axios from "axios"
 
 export const instance = axios.create({
     baseURL: 'https://uculture.onrender.com/api',
-    withCredentials: true
+    withCredentials: true,
+    headers:{
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+        'Content-Type': 'application/json',
+    }
 })
