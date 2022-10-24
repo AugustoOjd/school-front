83820989d9a347e7b2948cc0e72c90ad94f20b42
Augusto Ojeda <okarinex@gmail.com>
2022-10-24 14:37:51 -0300

    proxy v3

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: 'https://uculture.onrender.com/api',
+    baseURL: '/api',
     withCredentials: true,
     headers:{
         origin: "https://uculture.onrender.com/",
