1050977b392585e80ffd3b7662a762c4cbc6f775
Augusto Ojeda <okarinex@gmail.com>
2022-10-24 13:29:18 -0300

    cors v2

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -4,8 +4,6 @@ export const instance = axios.create({
     baseURL: 'https://uculture.onrender.com/api',
     withCredentials: true,
     headers:{
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
-        'Content-Type': 'application/json',
+        origin: "*",
     }
 })
