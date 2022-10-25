658bc5299e0026d39f2cf9f1a1e5da3bff549cd6
Augusto Ojeda <okarinex@gmail.com>
2022-10-25 09:19:18 -0300

    delete credentials

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -1,11 +1,5 @@
 import axios from "axios"
 
 export const instance = axios.create({
-    baseURL: 'https://uculture.onrender.com/api',
-    withCredentials: true,
-    headers: {
-        'Access-Control-Allow-Origin': 'https://uculture.onrender.com',
-        'Content-Type': 'application/json',
-    },
-    
+    baseURL: 'https://uculture.onrender.com/api'    
 })
