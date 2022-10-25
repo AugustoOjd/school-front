8f6ec70c2f8e01738717ace55cd183b417ffc0c0
Augusto Ojeda <okarinex@gmail.com>
2022-10-24 22:16:42 -0300

    headers

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -3,8 +3,9 @@ import axios from "axios"
 export const instance = axios.create({
     baseURL: 'https://uculture.onrender.com/api',
     withCredentials: true,
-    headers:{
-        origin: "https://uculture.onrender.com/",
-        header: "https://uculture.onrender.com/"
-    }
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    },
+    
 })
