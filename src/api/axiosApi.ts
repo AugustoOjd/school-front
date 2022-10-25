20ac6f4acdccacdbe27bc41a9ca96bdf5e7a32e0
Augusto Ojeda <okarinex@gmail.com>
2022-10-25 15:24:21 -0300

    edit axios again

--- src/api/axiosApi.ts
+++ src/api/axiosApi.ts
@@ -5,7 +5,6 @@ import Cookies from "js-cookie"
 export const instance = axios.create({
     baseURL: 'https://uculture.onrender.com/api',
     headers: {
-        "Content-Type": "application/json",
         token: Cookies.get('token')!,
         user:  Cookies.get('user')!
     },
